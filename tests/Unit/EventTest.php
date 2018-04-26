e2c1dbe83a7b35e77802cb31aed2282a7fac9c2b
Houssene Dao <dao.houssene@gmail.com>
2018-04-26 12:24:25 +0000

    create new test file

--- tests/Unit/EventTest.php
+++ tests/Unit/EventTest.php
@@ -1,1 +1,13 @@
 <?php
+
+use PHPUnit\Framework\TestCase;
+
+class EventTest extends TestCase
+{
+
+    /** @test */
+    public function sampleTest()
+    {
+        $this->assertTrue(true);
+    }
+}
